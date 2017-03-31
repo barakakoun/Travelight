f3c79396a1a8959301b961bd68cb546445036c33
ברק קקון < >
2017-03-31 03:54:29 +0300

    first navigator

--- index.android.js
+++ index.android.js
@@ -11,10 +11,15 @@ import {
     Text,
     View,
     Dimensions,
+    Navigator,
     TouchableOpacity,
     ToolbarAndroid
 } from 'react-native';
-import MapView from 'react-native-maps';
+
+import MainMapPage from './components/MainMapPage';
+import SplashPage from './components/SplashPage';
+import LoginPage from './components/LoginPage';
+import LoginPage from './components/TourDetailsPage';
 
 // import X from 'components/X';
 
@@ -38,232 +43,79 @@ function randomColor() {
 
 
 export default class travelight extends Component {
-    static title = '<ToolbarAndroid>';
-    static description = 'Examples of using the Android toolbar.';
-
-    state = {
-        actionText: 'Example app with toolbar component',
-        toolbarSwitch: false,
-        colorProps: {
-            titleColor: '#3b5998',
-            subtitleColor: '#6a7180',
-        },
-    };
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            tours: [
-                {
-                    key: 1,
-                    coordinate: {
-                        latitude: 32.0802627,
-                        longitude: 34.7808783
-                    },
-                    latDel: LATITUDE_DELTA,
-                    lonDel: LONGITUDE_DELTA
-                },
-                {
-                    key: 2,
-                    coordinate: {
-                        latitude: 32.0745575,
-                        longitude: 34.7772692
-                    },
-                    latDel: LATITUDE_DELTA,
-                    lonDel: LONGITUDE_DELTA
-                },
-                {
-                    key: 3,
-                    coordinate: {
-                        latitude: 32.0633612,
-                        longitude: 34.7730913
-                    },
-                    latDel: LATITUDE_DELTA,
-                    lonDel: LONGITUDE_DELTA
-                }
-            ],
-            region: {
-                latitude: LATITUDE,
-                longitude: LONGITUDE,
-                latitudeDelta: LATITUDE_DELTA,
-                longitudeDelta: LONGITUDE_DELTA
-            },
-            markers: [],
-        };
-    }
-
-    // onMapPress(e) {
-    //     this.setState({
-    //         markers: [
-    //             ...this.state.markers,
-    //             {
-    //                 coordinate: e.nativeEvent.coordinate,
-    //                 key: id++,
-    //                 color: randomColor(),
-    //             },
-    //         ],
-    //     });
-    // }
-
-    onTourPress(e) {
-        this.setState({
-            markers: [
-                ...this.state.markers,
-                {
-                    coordinate: e.nativeEvent.coordinate,
-                    key: id++,
-                    color: randomColor(),
-                },
-            ],
-        });
-    }
-
-
     render() {
         return (
-            <View style={styles.container}>
-              <ToolbarAndroid
-                  logo={baraklogo}
-                  title="AwesomeApp"
-                  actions={[{title: 'Settings', icon: baraklogo, show: 'always'}]}
-                  onActionSelected={this._onActionSelected} />
-
-
-                <MapView
-                  provider={this.props.provider}
-                  style={styles.map}
-                  initialRegion={this.state.region}
-              >
-                  {this.state.tours.map(marker => (
-                      <MapView.Marker
-                          key={marker.key}
-                          coordinate={marker.coordinate}
-                          image={barakpin}
-                          onMarkerPress={(e) => this.onTourPress(e)}
-                      />
-                  ))}
-              </MapView>
-
-              <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                    onPress={() => this.setState({ markers: [] })}
-                    style={styles.bubble}
-                >
-                  <Text style={{color:'blue'}}>TRAVELIGHT</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
+            <Navigator
+                initialRoute={{id: 'SplashPage', name: 'Index'}}
+                renderScene={this.renderScene.bind(this)}
+                configureScene={(route) => {
+            if (route.sceneConfig) {
+              return route.sceneConfig;
+            }
+            return Navigator.SceneConfigs.FloatFromRight;
+          }}/>
         );
     }
 
-    _onActionSelected = (position) => {
-        this.setState({
-            actionText: 'Selected ' + toolbarActions[position].title,
-        });
-    };
+        renderScene(route, navigator) {
+            var routeId = route.id;
+            if (routeId === 'SplashPage') {
+                return (
+                    <SplashPage
+                        navigator={navigator} />
+                );
+            }
+            if (routeId === 'MainMapPage') {
+                return (
+                    <MainMapPage
+                        navigator={navigator} />
+                );
+            }
+            if (routeId === 'LoginPage') {
+                return (
+                    <LoginPage
+                        navigator={navigator} />
+                );
+            }
+            if (routeId === 'TourDetailsPage') {
+                return (
+                    <LoginPage
+                        navigator={navigator} />
+                );
+            }
+            return this.noRoute(navigator);
+
+        }
+        noRoute(navigator) {
+            return (
+                <View style={{flex: 1, alignItems: 'stretch', justifyContent: 'center'}}>
+                    <TouchableOpacity style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
+                                      onPress={() => navigator.pop()}>
+                        <Text style={{color: 'red', fontWeight: 'bold'}}>s index.js ss renderScene sss</Text>
+                    </TouchableOpacity>
+                </View>
+            );
+        }
 }
 
-
-travelight.propTypes = {
-    provider: MapView.ProviderPropType,
-};
-
-
-
-var toolbarActions = [
-    {title: 'Create', icon: nativeImageSource({
-        android: 'ic_create_black_48dp',
-        width: 96,
-        height: 96
-    }), show: 'always'},
-    {title: 'Filter'},
-    {title: 'Settings', icon: nativeImageSource({
-        android: 'ic_settings_black_48dp',
-        width: 96,
-        height: 96
-    }), show: 'always'},
-];
-
-
-
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
     container: {
-        ...StyleSheet.absoluteFillObject,
-        justifyContent: 'flex-end',
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#F5FCFF',
     },
-    map: {
-        ...StyleSheet.absoluteFillObject,
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
     },
-    toolbar: {
-        backgroundColor: '#e9eaed',
-        height: 56,
-    },
-    bubble: {
-        backgroundColor: 'rgba(236,160,95,0.6)',
-        paddingHorizontal: 18,
-        paddingVertical: 12,
-        borderRadius: 20,
-    },
-    latlng: {
-        width: 200,
-        alignItems: 'stretch',
-    },
-    button: {
-        width: 80,
-        paddingHorizontal: 12,
-        alignItems: 'center',
-        marginHorizontal: 10,
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        marginVertical: 20,
-        backgroundColor: 'transparent',
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
     },
 });
 
-
-
-
-
-
-// const styles = StyleSheet.create({
-//     container: {
-//         position: 'absolute',
-//         top: 0,
-//         left: 0,
-//         right: 0,
-//         bottom: 0,
-//         justifyContent: 'flex-end',
-//         alignItems: 'center',
-//     },
-//     map: {
-//         position: 'absolute',
-//         top: 0,
-//         left: 0,
-//         right: 0,
-//         bottom: 0,
-//     },
-// });
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
 
 AppRegistry.registerComponent('travelight', () => travelight);
