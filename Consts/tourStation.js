d043edbbbd200d50a144bc912ce6ffee422cf796
tl1993 <tomer.laniado@gmail.com>
2017-04-16 20:43:48 +0300

    added new consts + select tours by city function

---
+++ Consts/tourStation.js
@@ -0,0 +1,6 @@
+/**
+ * Created by dell on 16/04/2017.
+ */
+exports.tourID = 'TOUR_ID';
+exports.stationID = 'STATION_ID';
+exports.stationNumber = 'STATION_NUMBER';
--- Server/api/tour/tour.controller.js
+++ Server/api/tour/tour.controller.js
@@ -4,11 +4,12 @@
 
 var db = require('../database');
 var tourConsts = require('../../../Consts/tour');
-
+var stationConsts = require('../../../Consts/station');
+var tourStationConsts = require('../../../Consts/tourStation');
+var messages = require('../../../Consts/messages');
 //create connection to mysql
 
-    exports.test = function(req, res)
-{
+exports.test = function(req, res) {
 
     //Connect to the DB
     connection = db.initDB();
@@ -24,7 +25,7 @@ var tourConsts = require('../../../Consts/tour');
         }
     });
     db.closeDB(connection)
-}
+};
 // Get tour by ID
 exports.getTourDetails = function(req,res) {
    var id =  req.param('tourId')
@@ -32,16 +33,93 @@ exports.getTourDetails = function(req,res) {
     connection = db.initDB();
     connection.query('SELECT * from tour where ID=?',id,function (err, rows, fields) {
         if (!err) {
-            console.log('The solution is: ', rows);
+
             res.send(rows);
         }
         else {
-            res.send('something weired happend');
-            console.log('Error while performing Query.');
+            res.send(messages.selectTourFailed);
+           throw err;
         }
     });
-}
+    db.closeDB(connection);
+};
+// Create Tour
 exports.create = function(req,res) {
-console.log(req.body);
-res.send('sds');
+var tour = req.body;
+//if(!tour.name||!tour.city||!tour.duration)
+if(!tour.name||!tour.duration)
+{
+    res.send('please enter all required fields');
+}
+else {
+    connection = db.initDB();
+    connection.query('INSERT INTO TOUR SET ?',tour,function(err,result) {
+        if(err) {
+            throw err;
+            res.send(messages.addTourFailed);
+        }
+        else
+        {
+            res.send(messages.addTourSuccess);
+        }
+    })
+    db.closeDB(connection);
+}
+
+};
+// Delete tour
+exports.delete = function(req,res) {
+    id = req.params.tourId;
+    console.log('delete id is'+id);
+    connection = db.initDB();
+    connection.query('DELETE FROM TOUR WHERE ID = ?',[id],function(err,result){
+        if(err)
+        {
+            throw err;
+            res.send(messages.deleteTourFailed);
+        }
+        else
+        {
+            res.send(messages.deleteTourSuccess);
+        }
+    })
+    db.closeDB(connection);
+};
+// Update tour
+exports.update = function(req,res) {
+    var id = req.param('tourId');
+    var tour = req.body;
+    connection = db.initDB();
+    connection.query('UPDATE TOUR SET ? WHERE ID=?',[tour,id],function (err,result) {
+       if(err)
+       {
+           throw err;
+           res.send(messages.updateTourFailed);
+       }
+       else
+       {
+           res.send(messages.updateTourSuceess);
+       }
+
+    });
+    db.closeDB(connection);
+};
+// Select tours by city and return first station location
+exports.selectByCity= function (req,res) {
+    var city = req.param('city');
+    console.log(city);
+    connection = db.initDB();
+    connection.query('SELECT T.ID,S.LATITUDE,S.LONGITUDE FROM TOUR T INNER JOIN TOUR_STATION TS ON T.ID = TS.TOUR_ID ' +
+        'INNER JOIN STATION S ON S.ID = TS.STATION_ID WHERE T.CITY = ? AND TS.STATION_NUMBER = 1',[city],function (err,rows,fields) {
+        if(err)
+        {
+            throw  err;
+            res.send(messages.selectTourFailed);
+        }
+        else
+        {
+            res.send(rows);
+        }
+    })
+    db.closeDB(connection);
 }
